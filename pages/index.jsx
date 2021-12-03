a5fb6adf78a0ceda815f5e12d43dd805864e70a0
coffmanjrp <coffmanjrp@gmail.com>
2021-12-02 16:40:36 -0800

    Initial setup

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Next Spotify</title>
+      </Head>
+
+      <h1>Hello World</h1>
+      <main>
+        {/* Sidebar */}
+        {/* Center */}
+      </main>
+      <div>{/* Player */}</div>
+    </div>
+  );
+}
